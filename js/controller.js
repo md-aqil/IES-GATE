9b3785fdc0d44877d509ac93fbb51971165cb7de
Md Aqil <aqilali381@gmail.com>
2020-06-03 18:10:14 +0530

    added

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,30 @@
 
+	
+	 
+//   tab
+
+$('.tabgroup > div').hide();
+
+$('.tabgroup > div:first-of-type').show();
+
+
+$('.tabs a').click(function(e){
+  e.preventDefault();
+	var $this = $(this),
+	
+        tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
+        others = $this.closest('li').siblings().children('a'),
+        target = $this.attr('href');
+    others.removeClass('active');
+    $this.addClass('active');
+    $(tabgroup).children('div').hide();
+    $(target).show();
+  
+})
+
+
+
+	
 	var swiper = new Swiper('.swiper-container.pcr', {
 		spaceBetween: 10,
 
@@ -241,28 +267,6 @@ ScrollOut({
 });
 
 
-
- 
-//   tab
-
-$('.tabgroup > div').hide();
-
-$('.tabgroup > div:first-of-type').show();
-
-
-$('.tabs a').click(function(e){
-  e.preventDefault();
-	var $this = $(this),
-	
-        tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
-        others = $this.closest('li').siblings().children('a'),
-        target = $this.attr('href');
-    others.removeClass('active');
-    $this.addClass('active');
-    $(tabgroup).children('div').hide();
-    $(target).show();
-  
-})
 
 
 
